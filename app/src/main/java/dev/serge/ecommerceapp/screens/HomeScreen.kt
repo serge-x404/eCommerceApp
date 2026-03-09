fdc4d7525a5e33f6313305702446833f9983464d
serge-x404 <kpdev1124@gmail.com>
2026-03-09 18:34:19 +0530

    Initial commit with topAppBar

---
+++ app/src/main/java/dev/serge/ecommerceapp/screens/HomeScreen.kt
@@ -0,0 +1,15 @@
+package dev.serge.ecommerceapp.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = { MyTopAppBar() }
+    ) { it ->
+        Modifier.padding(it)
+    }
+}
---
+++ app/src/main/java/dev/serge/ecommerceapp/screens/TopAppBar.kt
@@ -0,0 +1,51 @@
+package dev.serge.ecommerceapp.screens
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar() {
+    TopAppBar(
+        title = {
+            Text(
+                "eCommerceApp",
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = "Shopping Cart",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Profiles",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+    )
+}
