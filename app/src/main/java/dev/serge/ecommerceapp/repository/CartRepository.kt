a6431a34731a39705ad4fbd790f28f9664375d39
serge-x404 <kpdev1124@gmail.com>
2026-03-23 18:45:24 +0530

    CartRepo created

---
+++ app/src/main/java/dev/serge/ecommerceapp/repository/CartRepository.kt
@@ -0,0 +1,37 @@
+package dev.serge.ecommerceapp.repository
+
+import android.util.Log
+import android.widget.Toast
+import dev.serge.ecommerceapp.model.Product
+import dev.serge.ecommerceapp.room.CartDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CartRepository @Inject constructor(
+    private val cartDao: CartDao
+) {
+
+    val allCartItems: Flow<List<Product>> =
+        cartDao.getAllCartItems()
+
+    suspend fun addToCart(product: Product) {
+        val existingItems = cartDao.getCartItemById(product.id)
+
+        if (existingItems != null) {
+            cartDao.updateCartItem(product)
+            Log.v("TAGY","Product already added")
+        }
+        else {
+            cartDao.insertCartItem(product)
+            Log.v("TAGY","Product added!!")
+        }
+    }
+
+    suspend fun deleteFromCart(product: Product) {
+        cartDao.deleteCartItem(product)
+    }
+
+    suspend fun clearCart(product: Product) {
+        cartDao.clearCart()
+    }
+}
